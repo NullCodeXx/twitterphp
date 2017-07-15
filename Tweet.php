18b4102809ee649c19ee97168499852eaa16b408
cose_solo <djaafar.boumaza69@gmail.com>
2017-07-15 20:05:56 +0200

    maj file

--- Tweet.php
+++ Tweet.php
@@ -1,8 +1,8 @@
 <!--Class object instance-->
 <?php 
-//On definis class Tweets et tout ce qu'il y auras dedans les acolade seras reprenter par la classe.
+//On definis class Tweet et tout ce qu'il y auras dedans les acolade seras reprenter par la classe.
 class Tweet {
-    //les proprieter doivent avoir un visibiliter.$_COOKIE
+    //les proprieter doivent avoir un visibiliter.
     public $pseudo;
     public $message;
     public $date;
